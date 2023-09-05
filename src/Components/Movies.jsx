d89beaf759c2af3cbb646a34462717b497d1e0f8
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-05 10:57:34 +0300

    Adding Trailar for our website

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import "../Styles/Videos.css";
 import React, { Fragment, useEffect, useState, useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 import noImage from "./no-image.jpg";
 import { Container } from "./NavBar";
 
@@ -9,6 +10,7 @@ const Movies = () => {
   const [moviesData, setMoviesData] = useState([]);
   const [trailer, setTrailer] = useState(true);
   const { toggle, inputValue } = useContext(Container);
+  const [movieTitle, setMovieTitle] = useState("");
 
   const input = inputValue;
   const Shown = input ? "search" : "discover";
@@ -30,6 +32,11 @@ const Movies = () => {
     MovieCall();
   }, [input]); // Update by (every input) if Empty parameter to Run once the MovieCall function, to optimize the penformance ...
   console.log(moviesData);
+
+  const MoviesTitle = (movie) => {
+    setMovieTitle(movie.title);
+    setTrailer(!trailer);
+  };
   return (
     <Fragment>
       <div className={toggle ? "mainBgcolor" : "secondaryBgColor"}>
@@ -38,7 +45,12 @@ const Movies = () => {
             return (
               <Fragment>
                 <div id={trailer ? "container" : "NoContainer"}>
-                  <AiFillPlayCircle color="#fff" fontSize={40} id="playIcon" />
+                  <AiFillPlayCircle
+                    color="#fff"
+                    fontSize={40}
+                    id="playIcon"
+                    onClick={() => MoviesTitle(movie)}
+                  />
                   <img
                     src={
                       movie.poster_path
@@ -46,6 +58,7 @@ const Movies = () => {
                         : noImage
                     }
                     alt="no images"
+                    onClick={() => MoviesTitle(movie)}
                   />
                   <h3
                     id={movie.title.length > 28 ? "smaler-Text" : ""}
@@ -59,6 +72,14 @@ const Movies = () => {
           })}
         </div>
       </div>
+      <AiOutlineClose
+        id={trailer ? "Nothing" : "Exit1"}
+        className={toggle ? "DarkTheme" : "LightThemeClose"}
+        fontSize={55}
+        color="fff"
+        cursor={"pointer"}
+        onClick={(trailer) => setTrailer(true)}
+      />
     </Fragment>
   );
 };
--- src/Components/TvShows.jsx
+++ src/Components/TvShows.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { Fragment, useContext, useEffect, useState } from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+import { Container } from "./NavBar";
+import noImage from "./no-image.jpg";
+import axios from "axios";
 
 function TvShows() {
+  const { toggle, inputValue } = useContext(Container);
+  const [showData, setShowData] = useState([]);
+  const [trailer, setTrailer] = useState(true);
+  const [title, setTitle] = useState("");
+  const Api = " https://api.themoviedb.org/3/discover/tv";
+  const Images = "https://image.tmdb.org/t/p/w500/";
+
+  const TvShows = async () => {
+    const data = await axios.get(Api, {
+      params: {
+        api_key: "0f898ec93930a149724e4e4c3c310af8",
+        // query: input,
+      },
+    });
+    // To know our API key work or not .. console.log(data.data.results);
+    const results = data.data.results;
+    setShowData(results);
+    // console.log(showData);
+  };
+
+  useEffect(() => {
+    TvShows();
+  }, []); // Update by (every input) if Empty parameter to Run once the MovieCall function, to optimize the penformance ...
+  // console.log(showData)
+
+  const TvShowTitle = (shows) => {
+    setTitle(shows.name);
+    setTrailer(!trailer);
+  };
   return (
-    <div>TvShows</div>
-  )
+    <Fragment>
+      <div className={toggle ? "mainBgcolor" : "secondaryBgColor"}>
+        <div className="movies-container">
+          {showData.map((shows) => {
+            return (
+              <Fragment id={shows.id}>
+                <div id={trailer ? "container" : "NoContainer"}>
+                  <AiFillPlayCircle
+                    color="#fff"
+                    fontSize={40}
+                    id={trailer ? "playIcon" : "hide"}
+                    onClick={() => TvShowTitle(shows)}
+                  />
+                  <img
+                    src={
+                      shows.poster_path
+                        ? `${Images}${shows.poster_path}`
+                        : noImage
+                    }
+                    alt="no images"
+                    onClick={() => TvShowTitle(shows)}
+                  />
+                  <h3
+                    id={shows.name.length > 28 ? "smaler-Text" : ""}
+                    className={toggle ? "mainColor" : "secondaryColor"}
+                  >
+                    {shows.name}
+                  </h3>
+                </div>
+              </Fragment>
+            );
+          })}
+          <AiOutlineClose
+            id={trailer ? "Nothing" : "Exit1"}
+            className={toggle ? "DarkTheme" : "LightThemeClose"}
+            fontSize={55}
+            color="fff"
+            cursor={"pointer"}
+            onClick={(trailer) => setTrailer(true)}
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
-export default TvShows
+export default TvShows;
