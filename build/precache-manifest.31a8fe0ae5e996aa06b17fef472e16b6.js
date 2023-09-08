135474968d3e52af9dd1250e2426749b42b0b028
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-08 21:39:33 +0300

    Run build

---
+++ build/precache-manifest.31a8fe0ae5e996aa06b17fef472e16b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d32f00f835df51ca4204",
+    "url": "/static/css/main.92446bf3.chunk.css"
+  },
+  {
+    "revision": "d32f00f835df51ca4204",
+    "url": "/static/js/main.d32f00f8.chunk.js"
+  },
+  {
+    "revision": "3bf136ddf57acc53c7cf",
+    "url": "/static/js/1.3bf136dd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2ef26a8878ae022d715ece0cab336e3",
+    "url": "/static/media/no-image.c2ef26a8.jpg"
+  },
+  {
+    "revision": "b94f7b9da1a3938df7590806310bd4ce",
+    "url": "/index.html"
+  }
+];
